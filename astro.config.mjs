9b9b388055eaeeba98c90d18bbad38930e1c234b
ShunTr <repaco9@gmail.com>
2025-04-15 14:46:38 +0200

    First upload

---
+++ astro.config.mjs
@@ -0,0 +1,109 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: 'Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'CSS',
+                    collapsed: true,
+                    autogenerate: { directory: 'CSS' },
+                },
+                {
+                    label: 'Docker',
+                    collapsed: true,
+                    autogenerate: { directory: 'Docker' },
+                },
+                {
+                    label: 'English',
+                    collapsed: true,
+                    autogenerate: { directory: 'English' },
+                },
+                {
+                    label: 'GIT',
+                    collapsed: true,
+                    autogenerate: { directory: 'GIT' },
+                },
+                {
+                    label: 'Herramientas',
+                    collapsed: true,
+                    autogenerate: { directory: 'Herramientas' },
+                },
+                {
+                    label: 'HTML',
+                    collapsed: true,
+                    autogenerate: { directory: 'HTML' },
+                },
+                {
+                    label: 'JavaScript',
+                    collapsed: true,
+                    autogenerate: { directory: 'JavaScript' },
+                },
+                {
+                    label: 'MongoDB',
+                    collapsed: true,
+                    autogenerate: { directory: 'MongoDB' },
+                },
+                {
+                    label: 'Node.js',
+                    collapsed: true,
+                    autogenerate: { directory: 'Node.js' },
+                },
+                {
+                    label: 'NPM',
+                    collapsed: true,
+                    autogenerate: { directory: 'NPM' },
+                },
+                {
+                    label: 'PHP',
+                    collapsed: true,
+                    autogenerate: { directory: 'PHP' },
+                },
+                {
+                    label: 'Postgres',
+                    collapsed: true,
+                    autogenerate: { directory: 'Postgres' },
+                },
+                {
+                    label: 'React',
+                    collapsed: true,
+                    autogenerate: { directory: 'React' },
+                },
+                {
+                    label: 'Shell',
+                    collapsed: true,
+                    autogenerate: { directory: 'Shell' },
+                },
+                {
+                    label: 'VSCode',
+                    collapsed: true,
+                    autogenerate: { directory: 'VSCode' },
+                },
+                {
+                    label: 'Wordpress',
+                    collapsed: true,
+                    autogenerate: { directory: 'Wordpress' },
+                },
+
+                /*
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+*/
+            ],
+        }),
+    ],
+})
